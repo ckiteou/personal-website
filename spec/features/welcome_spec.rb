907b6848935e9fa12b0baba84985a87265cdb8f1
Costas Kiteou <ckiteou22@gmail.com>
2017-09-02 00:48:46 +0100

    Updated initial welcome spec test

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "Welcome Page:", type: :feature do
   scenario "Website has a welcome page" do
     visit "/"
-    expect(page).to have_content "Hello World!"
+    expect(page).to have_content "Home"
   end
 end
