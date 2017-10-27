2f8bbe71f470a0dc6576acb1a4cb108fff61948e
Costas Kiteou <ckiteou22@gmail.com>
2017-10-28 00:37:21 +0100

    Fixed functionality to send emails

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ActionMailer::Base
 
-  default from: "Gmail <noreply@gmail.com>"
+  default from: "Personal Website <ckiteou22@gmail.com>"
   default to: "Costas Kiteou <ckiteou22@gmail.com>"
 
   def new_message(message)
