0cd7902eeee696cc59eb50593e524091ed3b9f23
Costas Kiteou <ckiteou22@gmail.com>
2017-11-15 02:17:38 +0000

    Added gmail email and passwords to secret

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module PersonalWebsite
       address: "smtp.gmail.com",
       port: 587,
       domain: "gmail.com",
-      user_name: "ckiteou22@gmail.com",
-      password: "loggers0051",
+      user_name: Rails.application.secrets.user_name,
+      password: Rails.application.secrets.password,
       authentication: :plain,
       enable_starttls_auto: true
     }
