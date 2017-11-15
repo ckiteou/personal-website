c6f43a7f3bb17f9ab59a06a2abc66bb2f0605ebd
Costas Kiteou <ckiteou22@gmail.com>
2017-11-15 02:48:16 +0000

    Fixing heroku issue where email does not send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
+    user_name: ENV["GMAIL_SMTP_USER"],
+    password: ENV["GMAIL_SMTP_PASSWORD"],
     authentication: :plain,
     enable_starttls_auto: true
   }
