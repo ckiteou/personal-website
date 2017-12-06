23a37c1df886d5382c0216aecdf8eb2f52000e96
Costas Kiteou <ckiteou22@gmail.com>
2017-12-06 18:56:46 +0000

    Fixing heroku issue where email does not send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,10 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { :host => 'website-ckiteou-staging.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'website-ckiteou-staging.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.default_url_options = {
     host: "gmail.com"
