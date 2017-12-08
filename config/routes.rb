1590b24d5169e76e489967548a6e92d509b74563
Costas Kiteou <ckiteou22@gmail.com>
2017-12-08 19:38:59 +0000

    Added resume page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     resources :messages, controller: :contact
   end
   resources :portfolio
+  resources :resume
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
